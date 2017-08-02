c85834a01040580cb254e16e524cda6e802d589c
Dom Bonanni <dombonanni@gmail.com>
2017-08-02 13:37:25 -0400

    Added MdExpansionModule and cleaned up file for readability.

--- src/app/app-material-design.module.ts
+++ src/app/app-material-design.module.ts
@@ -1,15 +1,41 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MdListModule, MdCardModule, MdIconModule, MdInputModule, MdButtonModule, MdToolbarModule, MdSidenavModule, MdMenuModule } from '@angular/material';
+import {
+  MdListModule,
+  MdCardModule,
+  MdExpansionModule,
+  MdIconModule,
+  MdInputModule,
+  MdButtonModule,
+  MdToolbarModule,
+  MdSidenavModule,
+  MdMenuModule
+} from '@angular/material';
 
 @NgModule( {
   imports: [
-    CommonModule, MdListModule, MdCardModule, MdIconModule, MdInputModule, MdButtonModule, MdToolbarModule, MdSidenavModule, MdMenuModule
+    CommonModule,
+    MdListModule,
+    MdCardModule,
+    MdExpansionModule,
+    MdIconModule,
+    MdInputModule,
+    MdButtonModule,
+    MdToolbarModule,
+    MdSidenavModule,
+    MdMenuModule
   ],
   exports: [
-    MdListModule, MdCardModule, MdIconModule, MdInputModule, MdButtonModule, MdToolbarModule, MdSidenavModule, MdMenuModule
+    MdListModule,
+    MdCardModule,
+    MdExpansionModule,
+    MdIconModule,
+    MdInputModule,
+    MdButtonModule,
+    MdToolbarModule,
+    MdSidenavModule,
+    MdMenuModule
   ],
   declarations: []
 } )
 export class AppMaterialDesignModule {}
-
